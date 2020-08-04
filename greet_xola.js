f81a17393e55ac3aefb9f8311cada7ad3be3980b
WeHaveJoy <cnovuyo10@gmail.com>
2020-08-04 12:18:41 +0200

    new changes

--- greet_xola.js
+++ greet_xola.js
@@ -1,15 +1,36 @@
-// //import the greet module that is in the current folder
-// const greet = require('./greet');
-
-// console.log(greet('Xola'))
-
-
-
-
 
 const chalk = require('chalk');
+
 //import the greet module that is in the current folder
 const greet = require('./greet');
+const figlet = require('figlet');
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
+ const greeting = greet('Xola');
+figlet(greeting, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    //console.log(data)
+   const styledMessage = chalk.bgBlue.black((data));
 console.log(styledMessage)
+});
+
+figlet.text('Boo!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    //console.log(data);
+    const styledMessage = chalk.bgYellow.black((data));
+    console.log(styledMessage)
+});
+
